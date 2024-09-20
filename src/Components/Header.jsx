58445c2376905499b3a5c0154dc479d02478193a
archunar <you@example.com>
2024-09-20 22:35:43 +0530

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+// import './header.css';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>Header component</div>
-  )
+    <header className="header" data-testid="header">
+      <div className="left">
+        <Link to="/" style={{textDecoration: "none"}} data-testid="appTitleLink">
+          <h1 className='appTitle' data-testid="appTitle">Task Manager</h1>
+        </Link>
+      </div>
+      <div className="right">
+        <Link to="/newtask" style={{textDecoration: "none"}} data-testid="appTaskLink"> 
+          <p className="appTask" data-testid="appTask">Add Task</p>
+        </Link>
+      </div>
+    </header>
+  );  
 }
 
 export default Header;
