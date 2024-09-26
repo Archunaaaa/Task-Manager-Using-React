8c41cabdff0905ef91515cdf78db8c9ba310bf37
archunar <archunaramakrishnan@gmail.com>
2024-09-26 16:59:25 +0530

    add update task file completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Header from './Components/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import AddNewTask from './Components/AddNewTask';
-import Home from './Components/Page/NewTask';
+// import AddNewTask from './Components/AddNewTask';
+import Home from './Components/Page/Home';
+import NewTask from './Components/Page/NewTask';
 function App() {
   return (
     <Router> 
@@ -10,7 +11,7 @@ function App() {
         <Header />
         <Routes> 
         <Route exact path="/" element={<Home />} />
-          <Route path='/newtask' element={<AddNewTask />} /> 
+          <Route path='/newtask' element={<NewTask />} /> 
         </Routes>
       </div>
     </Router>
